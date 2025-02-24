6bf3b4b3aa7a0efac0dceee75cc2f3ddf81600ea
JamiroDV <vulderj@talnet.nl>
2025-02-24 17:58:59 +0100

    First Exercise made

---
+++ ConsoleApp1/Excercise1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//Opdracht: Maak een for loop waarbij je de naam en leeftijd van de gebruikers in de console schrijft.
+
+public class Exercise1
+{
+	public Exercise1()
+	{
+
+		List<User> users = new List<User>();
+        users.Add(new User("John", 25));
+        users.Add(new User("Jane", 22));
+
+        //Schrijf hier je loop
+
+
+    }
+
+
+}
+
+public class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public User(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
