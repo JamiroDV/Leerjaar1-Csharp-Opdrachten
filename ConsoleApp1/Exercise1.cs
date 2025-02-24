b0f5b7c0352dc5ca1b38698ac283df7b3fc00876
JamiroDV <vulderj@talnet.nl>
2025-02-24 18:07:13 +0100

    Update exercise 1

--- ConsoleApp1/Exercise1.cs
+++ ConsoleApp1/Exercise1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 //Opdracht: Maak een for loop waarbij je de naam en leeftijd van de gebruikers in de console schrijft.
 
@@ -6,14 +7,15 @@ public class Exercise1
 {
 	public Exercise1()
 	{
-
+        // lijst van de gebruikers
 		List<User> users = new List<User>();
+
+        //maakt een nieuwe user aan met naam en leeftijd
         users.Add(new User("John", 25));
         users.Add(new User("Jane", 22));
 
         //Schrijf hier je loop
-
-
+      
     }
 
 
